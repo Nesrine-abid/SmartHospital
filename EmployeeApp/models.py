6e793d96c2705c45385de3eed1f00e8cc912174c
nessrine <Nessrine.aloulou@enis.tn>
2022-04-14 21:29:28 +0100

    "passwordInput correction"

--- EmployeeApp/models.py
+++ EmployeeApp/models.py
@@ -6,8 +6,6 @@ from rest_framework.fields import ListField
 class Gender(Enum):
     MALE = 'male'
     FEMALE = 'female'
-
-
 class Address(models.Model):
     country = models.CharField(max_length=200)
     city = models.CharField(max_length=200)
@@ -19,7 +17,7 @@ class Information(models.Model):
     firstName = models.CharField(max_length=30)
     lastName = models.CharField(max_length=30)
     email = models.EmailField(max_length=254)
-    password = models.CharField(widget=models.PasswordInput)
+    password = models.CharField(max_length=254)
     phone = models.CharField(max_length=50)
     cin = models.CharField(max_length=8)
     passport = models.CharField(max_length=7)
