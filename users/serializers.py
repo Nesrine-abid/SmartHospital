af420d68fba6abb8c52e1b6c6bd0cdd7f24cd88b
nessrine <Nessrine.aloulou@enis.tn>
2022-06-02 12:33:18 +0100

    final

--- users/serializers.py
+++ users/serializers.py
@@ -132,13 +132,12 @@ class PatientSerializerForUpdate(serializers.ModelSerializer):
                   'country','street','allergy','occupation')
 class PatientSerializerForGetAll(serializers.ModelSerializer):
 
-
     class Meta:
         model = Patient
         fields = ('user_ptr_id','firstName',
                   'lastName', 'phone','nationality', 'date_of_Birth',
                   'gender','chronic_disease','city','postalCode',
-                  'country','street','allergy','occupation','file','staff_medical')
+                  'country','street','allergy','occupation','file','staff_medical','date_of_Birth')
 
 class PatientSerializer(serializers.ModelSerializer):
     information_ptr = InformationSerializer(required=False)
